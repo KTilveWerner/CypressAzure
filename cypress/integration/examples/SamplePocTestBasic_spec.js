af90130f61083709a991b4d1f44d0e903908c3ee
ktilvwerner <ktilve@werner.com>
2020-10-19 16:15:05 -0500

    First Commit

---
+++ cypress/integration/examples/SamplePocTestBasic_spec.js
@@ -0,0 +1,37 @@
+describe ('Verify User is able submit Need Authorization request', function()
+{
+    it('Verify Login Test', function()
+    {
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click() 
+       // cy.get('#item_5_title_link > div').click() //also you can use class name and index name 
+        
+        
+    })
+     
+    // it('Demo Asynchronous call', function()
+    // {
+    //     cy.visit('https://www.saucedemo.com/')
+    //     cy.get('#user-name').type('standard_user')
+    //     cy.get('#password').type('secret_sauce')
+    //     cy.get('#login-button').click() 
+    //     cy.get('#inventory_filter_container>div').should("have.text", "Products")
+    //     cy.get('#inventory_filter_container>div').then(function(heading){
+    //         var headingstr = heading.text()
+    //         cy.log('ssss',headingstr)
+    //         console.log('I am Inside')
+
+    //     })
+    //     console.log('I am Outside')
+    // })
+    
+
+    it('Verify Dropdown', function()
+    {
+        cy.visit('https://the-internet.herokuapp.com/dropdown') 
+       cy.get('#dropdown').select('Option 2')
+    })
+
+})
---
+++ cypress/integration/examples/alert.js
@@ -0,0 +1,24 @@
+describe('handling alerts', function(){
+
+    beforeEach(function() {
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+
+    })
+
+    it('Alert Type', function() {
+
+        cy.on('windows:alert', function(alertText){
+            expect(alertText).eq('I am a JS Alert')
+        })
+        cy.contains('Click for JS Alert').click()
+    })
+
+    it('Confirm Alert Type', function() {
+
+        cy.on('windows:confirm', function(confirmText){
+            return false
+        })
+        cy.contains('Click for JS Confirm').click()
+    })
+
+})
